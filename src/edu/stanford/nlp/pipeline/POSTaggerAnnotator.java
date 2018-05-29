5310e6f29031c5aa741a01440d3a8902ea53c62f
久保卓也 <>
2018-05-29 12:12:43 +0900

    added outer input path to the Postagger

--- src/edu/stanford/nlp/pipeline/POSTaggerAnnotator.java
+++ src/edu/stanford/nlp/pipeline/POSTaggerAnnotator.java
@@ -1,6 +1,7 @@
 package edu.stanford.nlp.pipeline; 
 import edu.stanford.nlp.util.logging.Redwood;
 
+import java.io.*;
 import java.util.*;
 
 import edu.stanford.nlp.ling.*;
@@ -8,6 +9,11 @@ import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.util.*;
 import edu.stanford.nlp.util.concurrent.MulticoreWrapper;
 import edu.stanford.nlp.util.concurrent.ThreadsafeProcessor;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.apache.http.protocol.HTTP.USER_AGENT;
 
 /**
  * Wrapper for the maxent part of speech tagger.
@@ -92,6 +98,7 @@ public class POSTaggerAnnotator implements Annotator  {
   @Override
   public void annotate(Annotation annotation) {
     // turn the annotation into a sentence
+    log.log("try to add pos tags");
     if (annotation.containsKey(CoreAnnotations.SentencesAnnotation.class)) {
       if (nThreads == 1) {
         for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
@@ -129,10 +136,33 @@ public class POSTaggerAnnotator implements Annotator  {
 
   private CoreMap doOneSentence(CoreMap sentence) {
     List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
-    List<TaggedWord> tagged = null;
-    if (tokens.size() <= maxSentenceLength) {
+    List<TaggedWord> tagged = new ArrayList<>();
+    int t_size = tokens.size();
+    if (t_size <= maxSentenceLength) {
       try {
-        tagged = pos.tagSentence(tokens, this.reuseTags);
+        log.log(sentence.toString());
+        log.log(tokens);
+        String sent = "";
+        List<CoreLabel> tokens_ = tokens.subList(0, t_size - 1);
+        for (CoreLabel token: tokens_){
+          sent += token.word() + " ";
+        }
+        sent += tokens.get(t_size - 1).word() + "\n";
+        log.log(sent);
+        String ret = null;
+        try {
+          ret = this.sendPost(sent);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        log.log(ret);
+        String[] tagged_ = ret.split("\\s+");
+        // tagged = pos.tagSentence(tokens, this.reuseTags);
+        for (String token:tagged_) {
+          String[] tg = token.split("/");
+          tagged.add(new TaggedWord(tg[0], tg[1]));
+        }
+        log.log(tagged);
       } catch (OutOfMemoryError e) {
         log.error(e); // Beware that we can now get an OOM in logging, too.
         log.warn("Tagging of sentence ran out of memory. " +
@@ -169,4 +199,40 @@ public class POSTaggerAnnotator implements Annotator  {
     return Collections.singleton(CoreAnnotations.PartOfSpeechAnnotation.class);
   }
 
+  private String sendPost(String text) throws Exception {
+
+    String url = "http://localhost:5000";
+    URL obj = new URL(url);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+    //add reuqest header
+    con.setRequestMethod("POST");
+    con.setRequestProperty("User-Agent", USER_AGENT);
+    con.setRequestProperty("content-language", "en");
+    con.setRequestProperty("content-type", "text/plain");
+    con.setRequestProperty("accept-charset", "utf-8");
+    con.setRequestProperty("cache-control", "no-cache");
+
+
+    // Send post request
+    con.setDoOutput(true);
+    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+    wr.write(text);
+    wr.flush();
+    wr.close();
+
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
+    }
+    in.close();
+
+    //print result
+    return response.toString();
+
+  }
 }
